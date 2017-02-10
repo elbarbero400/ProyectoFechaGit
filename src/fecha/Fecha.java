e17f93b9ea2bd9c9b6f95a7d12f97b814540f129
203pc19 <203pc19@203p13-PC>
2017-02-10 09:55:17 +0100

    Tercer Punto

--- src/fecha/Fecha.java
+++ src/fecha/Fecha.java
@@ -1,5 +1,8 @@
 package fecha;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author Mario
@@ -14,6 +17,14 @@ public class Fecha {
         this.dias = dias;
         this.mes = mes;
         this.anno = anno;
+    }
+
+    public Fecha() {
+        /*Con esta clase (la clase Calendar) que ya tiene java incluida, tomamos la fecha actual del sistema.*/
+        Calendar c = new GregorianCalendar();
+        dias = c.get(Calendar.DAY_OF_MONTH);
+        mes = c.get(Calendar.MONTH);
+        anno = c.get(Calendar.YEAR);
     }
 
 }
